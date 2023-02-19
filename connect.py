b502d33ff5f01529f6ffa3657804fe02e7a54cd1
Zeanderson <121685062+Zeanderson@users.noreply.github.com>
2023-02-19 12:14:51 -0600

    hello world
    Hello world commits

--- connect.py
+++ connect.py
@@ -1,34 +1,53 @@
-import psycpog2
+import flask
+import os
+from flask import send_from_directory
 
-class Adaptor():
-  def __init__(self):
-    pass
+app = flask.Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+  return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype = 'image/favicon.png')
+
+@app.route('/')
+@app.route('home')
+def home():
+  return "Hello World"
+
+  if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
+
+# import psycpog2
+
+# class Adaptor():
+#   def __init__(self):
+#     pass
   
-  def create(self, first, last, code, playerID):
-    self.first = first
-    self.last = last
-    self.code = code
-    self.playerID = playerID
+#   def create(self, first, last, code, playerID):
+#     self.first = first
+#     self.last = last
+#     self.code = code
+#     self.playerID = playerID
     
-    try:
-      myconnect = psycopg2.connect(dbname="", user="", password="", host="")
-      mycursor = myconnect.cursor()
+#     try:
+#       myconnect = psycopg2.connect(dbname="", user="", password="", host="")
+#       mycursor = myconnect.cursor()
       
-    except:
-       print("Can't connect")
+#     except:
+#        print("Can't connect")
         
-    entry = "INSERT INTO player (first, last, code, playerID) VALUES (%s, %s, %s, %s)"
-    playerValues = (self.first, self.last, self.code, self.playerID)
+#     entry = "INSERT INTO player (first, last, code, playerID) VALUES (%s, %s, %s, %s)"
+#     playerValues = (self.first, self.last, self.code, self.playerID)
     
-    try:
-      mycursor.execute(entry, playerValues)
-      myconnect.commit()
+#     try:
+#       mycursor.execute(entry, playerValues)
+#       myconnect.commit()
     
-    except:
-      print("Error in adding player")
+#     except:
+#       print("Error in adding player")
     
     
-    myconnect.close()
+#     myconnect.close()
     
-    return 1
-                               
+#     return 1
